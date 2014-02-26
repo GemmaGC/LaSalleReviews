dade0f8a04ad7a7c23824dd1508dea62bf5dd7c4
Claudia Daudén <cldauden@gmail.com>
2014-02-26 19:17:47 +0100

    no message

--- templates_c/%%49^492^492A5AA1%%micos.tpl.php
+++ templates_c/%%49^492^492A5AA1%%micos.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.14, created on 2014-02-26 19:11:51
+<?php /* Smarty version 2.6.14, created on 2014-02-26 19:16:44
          compiled from exercici1/micos.tpl */ ?>
 
     <!-- Això és un comentari HTML -->
@@ -9,48 +9,50 @@
             <meta charset="UTF-8" />
             <title>Fotografies dels micos</title>
 
-
-
             <link rel="stylesheet" type="text/css" href="<?php echo $this->_tpl_vars['url']['global']; ?>
 /css/stylemicos.css" />
 
         </head>
 
         <body>
-        <div class="main_header">
-            <header>
-                <div class="site-logo">EXERCICI 1</div>
-            </header>
-        </div>
-        <div id="container">
+
+            <div class="main_header">
+                <header>
+                    <div class="site-logo">EXERCICI 1</div>
+                </header>
+            </div>
 
 
-            <section>
-                <div class="clr"></div>
-                <!--<figure class="container_buttons">-->
+            <div id="container">
+
+                <section>
+                    <div class="clr"></div>
+
                     <img class="img" id="img" src="<?php echo $this->_tpl_vars['url']['global']; ?>
 /imag/exercici1/<?php echo $this->_tpl_vars['act_img']; ?>
 .jpg">
-                <!--</figure>-->
-                <div class="container_buttons">
 
-                    <?php if ($this->_tpl_vars['prev_img'] > 0): ?>
-                        <a class="prev" href="<?php echo $this->_tpl_vars['url']['global']; ?>
+                    <div class="container_buttons">
+
+                        <?php if ($this->_tpl_vars['prev_img'] > 0): ?>
+                            <a class="prev" href="<?php echo $this->_tpl_vars['url']['global']; ?>
 /micos/<?php echo $this->_tpl_vars['prev_img']; ?>
 ">Anterior</a>
-                    <?php endif; ?>
-                    <?php if ($this->_tpl_vars['next_img'] < 11): ?>
-                        <a class="next" href="<?php echo $this->_tpl_vars['url']['global']; ?>
+                        <?php endif; ?>
+                        <?php if ($this->_tpl_vars['next_img'] < 11): ?>
+                            <a class="next" href="<?php echo $this->_tpl_vars['url']['global']; ?>
 /micos/<?php echo $this->_tpl_vars['next_img']; ?>
 ">Següent</a>
-                    <?php endif; ?>
-                </div>
-            </section>
+                        <?php endif; ?>
+                    </div>
+                </section>
 
-        </div>
-        <footer>
-            <p>La Salle - Universitat Ramon Llull - curs 2013 / 2014</p>
-        </footer>
+            </div>
+
+
+            <footer>
+                <p>La Salle - Universitat Ramon Llull - curs 2013 / 2014</p>
+            </footer>
 
         </body>
 
