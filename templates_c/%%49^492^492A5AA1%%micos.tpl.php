6439f3e1ec08a8beea2e97d20a51e4d3158f1e0a
Claudia Daudén <cldauden@gmail.com>
2014-02-26 18:52:53 +0100

    FUNCIONEN LES FOTOS!!

--- templates_c/%%49^492^492A5AA1%%micos.tpl.php
+++ templates_c/%%49^492^492A5AA1%%micos.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.14, created on 2014-02-26 18:47:48
+<?php /* Smarty version 2.6.14, created on 2014-02-26 18:50:59
          compiled from exercici1/micos.tpl */ ?>
 
     <!-- Això és un comentari HTML -->
@@ -28,17 +28,20 @@
             <section>
                 <div class="clr"></div>
                 <figure class="container_buttons">
-                    <img class="img" src="imag/exercici1/<?php echo $this->_tpl_vars['info']; ?>
+                    <img class="img" src="<?php echo $this->_tpl_vars['url']['global']; ?>
+/imag/exercici1/<?php echo $this->_tpl_vars['act_img']; ?>
 .jpg">
                 </figure>
                 <div class="container_buttons">
 
                     <a class="prev" href="<?php echo $this->_tpl_vars['url']['global']; ?>
-/micos/">Anterior</a>
+/micos/<?php echo $this->_tpl_vars['prev_img']; ?>
+">Anterior</a>
 
 
                     <a class="next" href="<?php echo $this->_tpl_vars['url']['global']; ?>
-/micos/">Següent</a>
+/micos/<?php echo $this->_tpl_vars['next_img']; ?>
+">Següent</a>
 
                 </div>
             </section>
