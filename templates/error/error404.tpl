a649388b758d675d0ecd67b4fbb804dc3bfdb161
Gemma <geemmaa.05@gmail.com>
2014-02-27 12:04:12 +0100

    canvi css pagina error *.*

--- templates/error/error404.tpl
+++ templates/error/error404.tpl
@@ -1,3 +1,3 @@
 {$modules.head}
-Página 404
+<p class="error"><img class="alert" src="imag/alert.svg">    <strong>Pàgina 404.</strong><br/>Sembla que has intentat entrar en una pàgina que no existeix!</p>
 {$modules.footer}
--- templates_c/%%F8^F8A^F8A868DB%%error404.tpl.php
+++ templates_c/%%F8^F8A^F8A868DB%%error404.tpl.php
@@ -1,6 +1,6 @@
-<?php /* Smarty version 2.6.14, created on 2014-02-24 17:26:58
+<?php /* Smarty version 2.6.14, created on 2014-02-27 11:57:35
          compiled from error/error404.tpl */ ?>
 <?php echo $this->_tpl_vars['modules']['head']; ?>
 
-Página 404
+<p class="error"><img class="alert" src="imag/alert.svg">    <strong>Pàgina 404.</strong><br/>Sembla que has intentat entrar en una pàgina que no existeix!</p>
 <?php echo $this->_tpl_vars['modules']['footer']; ?>
