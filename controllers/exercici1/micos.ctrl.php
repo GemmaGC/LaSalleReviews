fab30585a1b3e0530013ed50fabcde12e1d71d49
Claudia Daudén <cldauden@gmail.com>
2014-02-27 19:33:50 +0100

    no message

--- controllers/exercici1/micos.ctrl.php
+++ controllers/exercici1/micos.ctrl.php
@@ -5,35 +5,10 @@
 class Exercici1MicosController extends Controller
 {
     protected $view = 'exercici1/micos.tpl';
-    protected $view2 = 'error/error404.tpl';
 
     public function build()
     {
-        $info = $this->getParams();
-        /*echo '<pre>';print_r( $info );echo '</pre>';
-
-        if($info[url_arguments][0] < 1 || $info[url_arguments][0] > 10){
-            $this->assign('img', $info[url_arguments][0]);
-            echo '<pre>';print_r( $this->view );echo '</pre>';
-            $this->setLayout($this->view);
-        }else{
-            $this->setLayout($this->view2);
-            echo '<pre>';print_r( $this->view2 );echo '</pre>';
-        }*/
         $this->setLayout($this->view);
     }
 
-    /**
-     * With this method you can load other modules that we will need in our page. You will have these modules availables in your template inside the "modules" array (example: {$modules.head}).
-     * The sintax is the following:
-     * $modules['name_in_the_modules_array_of_Smarty_template'] = Controller_name_to_load;
-     *
-     * @return array
-     */
-    /*public function loadModules() {
-        $modules['head']	= 'SharedHeadController';
-        $modules['footer']	= 'SharedFooterController';
-        return $modules;
-    }
-    */
 }
--- templates_c/%%F8^F8A^F8A868DB%%error404.tpl.php
+++ templates_c/%%F8^F8A^F8A868DB%%error404.tpl.php
@@ -1,6 +1,7 @@
-<?php /* Smarty version 2.6.14, created on 2014-02-27 11:57:35
+<?php /* Smarty version 2.6.14, created on 2014-02-27 19:31:48
          compiled from error/error404.tpl */ ?>
 <?php echo $this->_tpl_vars['modules']['head']; ?>
 
-<p class="error"><img class="alert" src="imag/alert.svg">    <strong>Pàgina 404.</strong><br/>Sembla que has intentat entrar en una pàgina que no existeix!</p>
+<p class="error"><img class="alert" src="<?php echo $this->_tpl_vars['url']['global']; ?>
+/imag/alert.svg">    <strong>Pàgina 404.</strong><br/>Sembla que has intentat entrar en una pàgina que no existeix!</p>
 <?php echo $this->_tpl_vars['modules']['footer']; ?>
