93bb1e8d5683fef28937f2b6dd53d8b1e5b8a892
Claudia Daudén <cldauden@gmail.com>
2014-02-27 19:30:13 +0100

    no message

--- controllers/exercici1/botons.ctrl.php
+++ controllers/exercici1/botons.ctrl.php
@@ -18,14 +18,14 @@ class Exercici1BotonsController extends Controller
         $this->assign('next_img', $info[url_arguments][0] +1);
 
         if($info[url_arguments][0] < 1 || $info[url_arguments][0] > 10){
+            $this->setLayout($this->view2);
+            echo '<pre>';print_r( $info );echo '</pre>';
+        }else{
             $this->setLayout($this->view);
 
             $this->assign('act_img', $info[url_arguments][0]);
             $this->assign('prev_img', $info[url_arguments][0] -1);
             $this->assign('next_img', $info[url_arguments][0] +1);
-            echo '<pre>';print_r( $info );echo '</pre>';
-        }else{
-            $this->setLayout($this->view2);
             echo '<pre>';print_r( $info );echo '</pre>';
         }
 
