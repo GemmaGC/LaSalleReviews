b19c8bc21cca3cebdaa8b4757dd70798da31616d
Ylenia <aily.leni@gmail.com>
2014-03-13 12:28:00 +0100

    Insertar Imagenes ejercicio 3

---
+++ templates_c/%%C7^C7B^C7B6D054%%afegeix.tpl.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version 2.6.14, created on 2014-03-10 16:47:21
+         compiled from exercici3/afegeix.tpl */ ?>
+<?php echo $this->_tpl_vars['modules']['head']; ?>
+
+
+    <div class="go_back_container_micos">
+        <a class="prev" href="<?php echo $this->_tpl_vars['enrere']; ?>
+">Enrere</a>
+    </div>
+
+
+    <div class="fotos_container">
+        <?php echo $this->_tpl_vars['modules']['formulari']; ?>
+
+        <?php echo $this->_tpl_vars['modules']['formulari']; ?>
+
+        <?php echo $this->_tpl_vars['modules']['formulari']; ?>
+
+    </div>
+
+<?php echo $this->_tpl_vars['modules']['footer']; ?>
+
+
+
