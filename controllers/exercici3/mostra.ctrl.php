6fa39937c44edb7fa1c98600920e06a39cd798a1
Claudia Daudén <cldauden@gmail.com>
2014-03-16 19:37:01 +0100

    url controlada!

--- controllers/exercici3/mostra.ctrl.php
+++ controllers/exercici3/mostra.ctrl.php
@@ -18,11 +18,6 @@ class Exercici3MostraController extends Controller
 
         $info = $this->getParams();
 
-        $this->assign('header', 'EXERCICI 3');
-
-        $this->assign('enrere', '/exercici3');
-
-        $this->setParams( array( 'num' => $info['url_arguments'][0] ) );
 
         //És la forma més cutre de calcular el màxim de pàgines, ja ho optimitzarem
         $model = $this->getClass( 'Exercici3GestorModel' ); //Importem el model
@@ -31,20 +26,34 @@ class Exercici3MostraController extends Controller
         $orn = count($model->getImatges('ornitorrincos'));
 
         $max = max($marm, $mon, $orn);
+        $min = 0;
+
+        if(is_numeric($info['url_arguments'][0]) && $info['url_arguments'][0] >= $min && $info['url_arguments'][0] < $max+1 && count($info['url_arguments'])==1){
+
+            $this->assign('header', 'EXERCICI 3');
+            $this->assign('enrere', '/exercici3');
+
+            $this->setParams( array( 'num' => $info['url_arguments'][0] ) );
+
+            $this->assign('min', $min);
+            $this->assign('max', $max);
+            $this->assign('num', $info['url_arguments'][0]);
+
+            $this->assign('boto_ant', '/imag/botons/enrere.png');
+            $this->assign('url_ant', $info['url_arguments'][0]-3);
+
+            $this->assign('boto_seg', '/imag/botons/seg.png');
+            $this->assign('url_seg', $info['url_arguments'][0]+3);
 
 
-        $this->assign('min', 0);
-        $this->assign('max', $max);
-        $this->assign('num', $info['url_arguments'][0]);
-
-        $this->assign('boto_ant', '/imag/botons/enrere.png');
-        $this->assign('url_ant', $info['url_arguments'][0]-3);
-
-        $this->assign('boto_seg', '/imag/botons/seg.png');
-        $this->assign('url_seg', $info['url_arguments'][0]+3);
+            $this->setLayout($this->view);
 
 
-        $this->setLayout($this->view);
+        }else{
+            $this->setLayout($this->view2);
+
+
+        }
 
 
     }
