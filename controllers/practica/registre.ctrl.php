c94bd91e1c5a0112cc5456e4fa7e9f92be33f89d
Claudia Daudén <cldauden@gmail.com>
2014-04-04 17:45:43 +0200

    Revert "empty"
    This reverts commit 4010867dcb0a69a08bc5cc1043e852b5ad199a3d.

--- controllers/practica/registre.ctrl.php
+++ controllers/practica/registre.ctrl.php
@@ -135,7 +135,6 @@ class PracticaRegistreController extends Controller
                 //$this->assign('val', 1);
 
                 $this->assign('vMail', 1);
-                llll
 
                 $this->assign('nom', $usuari['nom']);
                 $this->assign('login', $usuari['login']);
