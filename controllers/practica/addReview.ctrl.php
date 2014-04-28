0009016b12115cfbf2e393b69d932ec0aa606a20
Ylenia <aily.leni@gmail.com>
2014-04-28 17:01:47 +0200

    comprovacio de camp

--- controllers/practica/addReview.ctrl.php
+++ controllers/practica/addReview.ctrl.php
@@ -23,12 +23,13 @@ class PracticaAddReviewController extends Controller
                 $review['date'] = Filter::getString('newDate');
                 $review['score'] = Filter::getInteger('newScore');
                 $review['image'] = Filter::getString('newImage');
-                $this->assign('vtitle', 0); $this->assign('vdescription', 0);
-                $this->assign('vsubject', 0); $this->assign('vdate', 0); $this->assign('score', 0); $this->assign('image', 0);
+                $this->assign('vtitle', 0); $this->assign('description', 0);
+                $this->assign('subject', 0); $this->assign('date', 0);
+                $this->assign('score', 0); $this->assign('vimage', 0);
 
 
 
-                //if (PracticaAddReviewController::comprovaCamps($review)){
+               if (PracticaAddReviewController::comprovaCamps($review)){
 
                 $this->model->afegeixReview($review['title'], $review['description'],$review['subject'], $review['date'], $review['score'], $review['image']);
 
@@ -39,7 +40,7 @@ class PracticaAddReviewController extends Controller
                 Session::getInstance()->set('score', $review['score']);
                 Session::getInstance()->set('image', $review['image']);
 
-                //}
+                }
 
                 /****** ENVIAR MAIL AMB CODI D'ACTIVACIÓ DE COMPTE *********/
             }
@@ -47,17 +48,16 @@ class PracticaAddReviewController extends Controller
         }
     }
 
-   /* protected function comprovaCamps($review)
+    protected function comprovaCamps($review)
     {
         $reviews = $this->model->getTot('review');
         $var = true;
 
-        foreach($reviews as $u)
+        foreach($reviews as $r)
         {
-            if(!strcmp($u['title'], $review['title'])|| strlen($review['title']) < 100) //Comprovem que el nom d'usuari sigui únic
+            if(!strcmp($r['title'], $review['title'])|| strlen($review['title']) > 100) //Comprovem que el titol sigui inferior a 100 caracters
             {
                 $this->assign('vtitle', 1);
-
                 $this->assign('title', $review['title']);
                 $this->assign('description', $review['description']);
                 $this->assign('subject', $review['subject']);
@@ -69,43 +69,33 @@ class PracticaAddReviewController extends Controller
                 $var = false;
             }
 
-            if(!strcmp($u['login'], $usuari['login']) || PracticaRegistrecontroller::comprovaLogin($u['login'])) //Comprovem que el login sigui unic, tingui una llargada de 7, 2 lletres i 2 num
-            {
 
-                $this->assign('vLogin', 1);
 
-                $this->assign('nom', $usuari['nom']);
-                $this->assign('login', $usuari['login']);
-                $this->assign('email', $usuari['email']);
-                $this->assign('password', $usuari['password']);
-                $var = false;
-            }
 
-            if(!strcmp($u['email'], $usuari['email'])) //Comprovem que l'email sigui únic
-            {
-                $this->assign('vMail', 1);
-
-                $this->assign('nom', $usuari['nom']);
-                $this->assign('login', $usuari['login']);
-                $this->assign('email', $usuari['email']);
-                $this->assign('password', $usuari['password']);
-                $var = false;
-            }
         }
 
-        if(strlen($usuari['password']) < 6 || strlen($usuari['password']) > 20 ) //Comprovem que la contrassenya tingui entre 6 i 20 caràcters
-        {
-            $this->assign('vPas', 1);
+        //if(strlen($usuari['password']) < 6 || strlen($usuari['password']) > 20 ) //Comprovem que la contrassenya tingui entre 6 i 20 caràcters
+      //  {
+        $this->assign('vimage', 1);
 
-            $this->assign('nom', $usuari['nom']);
-            $this->assign('login', $usuari['login']);
-            $this->assign('email', $usuari['email']);
-            $this->assign('password', $usuari['password']);
-            $var = false;
-        }
+        $this->assign('title', $review['title']);
+        $this->assign('description', $review['description']);
+        $this->assign('subject', $review['subject']);
+        $this->assign('date', $review['date']);
+        $this->assign('score', $review['score']);
+        $this->assign('image', $review['image']);
+         $var = true;
+        //}
         return $var;
+
+        // imagesx($img);
+        // imagesy($img);
+        //rescalar imagen
+       //resource imagescale ( resource $image , int $new_width [, int $new_height = -1 [, int $mode = IMG_BILINEAR_FIXED ]] )
     }
-*/
+
+
+
 
     public function loadModules() {
 
