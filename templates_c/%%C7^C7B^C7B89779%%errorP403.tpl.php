a53a15932f4524eef2956805afcbb6f5df924565
Gemma <geemmaa.05@gmail.com>
2014-05-10 16:32:04 +0200

    canvi error 403

---
+++ templates_c/%%C7^C7B^C7B89779%%errorP403.tpl.php
@@ -0,0 +1,13 @@
+<?php /* Smarty version 2.6.14, created on 2014-05-10 16:29:59
+         compiled from practica/error/errorP403.tpl */ ?>
+<?php echo $this->_tpl_vars['modules']['headPractica']; ?>
+
+
+    <div class="error">
+        <div class="bloc_error_403">ACCESS DENIED.</div>
+        <p><strong>Ooops! Error 403.</strong><br/>Sorry but you are not authorized to access this page.</p>
+    </div>
+
+    <div class="omplir"></div>
+
+<?php echo $this->_tpl_vars['modules']['footerPractica']; ?>
