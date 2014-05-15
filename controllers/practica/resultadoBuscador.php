f34177c85ad128054d3b173831d6f7f86c8b0a16
Ylenia <aily.leni@gmail.com>
2014-05-15 11:35:11 +0200

    dgfb

--- controllers/practica/resultadoBuscador.php
+++ controllers/practica/resultadoBuscador.php
@@ -6,6 +6,11 @@ class PracticaResultadoBuscadorController extends Controller {
 
     public function build( ){
         $this->model = $this->getClass( 'PracticaReviewModel' ); //Importem el model
+
+        $palabra = Filter::getString('palabra');
+        $filter = Filter::getString('filter');
+        $url = $this->getParams();
+
         //Recuperem totes les review
         $reviews = $this->model->get10R();
         $this->assign('reviews', $reviews);
